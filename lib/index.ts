bc514994dc3aa9a1f22572fba6965699410aa610
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-01 20:25:59 -0800

    Properly clean up after myself

--- lib/index.ts
+++ lib/index.ts
@@ -1,34 +1,47 @@
+import {CompositeDisposable} from 'atom'
+
 import PackageSync from './package-sync'
+
+let disposable: CompositeDisposable
 
 /**
  * Activates the package.
  */
 export function activate(): void {
+  disposable = new CompositeDisposable()
+
   let packageSync = new PackageSync()
 
-  atom.commands.add('atom-workspace', 'package-sync:create-package-list', () => {
+  disposable.add(atom.commands.add('atom-workspace', 'package-sync:create-package-list', () => {
     packageSync.createPackageList()
-  })
+  }))
 
-  atom.commands.add('atom-workspace', 'package-sync:open-package-list', () => {
+  disposable.add(atom.commands.add('atom-workspace', 'package-sync:open-package-list', () => {
     packageSync.openPackageList()
-  })
+  }))
 
-  atom.commands.add('atom-workspace', 'package-sync:sync', () => {
+  disposable.add(atom.commands.add('atom-workspace', 'package-sync:sync', () => {
     packageSync.sync()
-  })
+  }))
 
-  atom.packages.onDidActivateInitialPackages(() => {
-    atom.packages.onDidLoadPackage(() => {
+  disposable.add(atom.packages.onDidActivateInitialPackages(() => {
+    disposable.add(atom.packages.onDidLoadPackage(() => {
       if (atom.config.get('package-sync.createOnChange')) {
         packageSync.createPackageList()
       }
-    })
+    }))
 
-    atom.packages.onDidUnloadPackage(() => {
+    disposable.add(atom.packages.onDidUnloadPackage(() => {
       if (atom.config.get('package-sync.createOnChange')) {
         packageSync.createPackageList()
       }
-    })
-  })
+    }))
+  }))
+}
+
+/**
+ * Deactivates the package.
+ */
+export function deactivate(): void {
+  disposable.dispose()
 }

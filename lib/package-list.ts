ad3f6476266292d574bbe2234b29e944028b376d
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-01 17:29:10 -0800

    :memo:

--- lib/package-list.ts
+++ lib/package-list.ts
@@ -6,13 +6,22 @@ interface PackagesFile {
   packages: string[]
 }
 
+/**
+ * Represents the stored package list.
+ */
 export default class PackageList {
   static packageListPath = path.join(atom.getConfigDirPath(), 'packages.cson')
 
+  /**
+   * Gets the path to the stored package list.
+   */
   static getPackageListPath(): string {
     return this.packageListPath
   }
 
+  /**
+   * Gets the packages from the list.
+   */
   getPackages(): string[] {
     if (fs.existsSync(PackageList.getPackageListPath())) {
       let obj = CSON.readFileSync(PackageList.getPackageListPath()) as PackagesFile
@@ -22,6 +31,10 @@ export default class PackageList {
     return []
   }
 
+  /**
+   * Updates the stored package list with what is currently installed if the list doesn't exist or
+   * the `forceOverwrite` configuration option is set to `true`.
+   */
   setPackages(): void {
     if (atom.config.get('package-sync.forceOverwrite') || !fs.existsSync(PackageList.getPackageListPath())) {
       let available = atom.packages.getAvailablePackageNames()
